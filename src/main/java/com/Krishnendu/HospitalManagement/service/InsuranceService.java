d2d3e778079ab8623a003a7621dee6acbe5bb132
Krishnendu De <krishnendu1205.be22@chitkarauniversity.edu.in>
2026-01-27 18:57:10 +0530

    Created initial structure of the insurance service

---
+++ src/main/java/com/Krishnendu/HospitalManagement/service/InsuranceService.java
@@ -0,0 +1,30 @@
+package com.Krishnendu.HospitalManagement.service;
+
+import com.Krishnendu.HospitalManagement.model.Insurance;
+import com.Krishnendu.HospitalManagement.model.Patient;
+import com.Krishnendu.HospitalManagement.repository.InsuranceRepo;
+import com.Krishnendu.HospitalManagement.repository.PatientRepo;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class InsuranceService {
+
+    public InsuranceService(InsuranceRepo insuranceRepo, PatientRepo patientRepo) {
+        this.insuranceRepo = insuranceRepo;
+        this.patientRepo = patientRepo;
+    }
+
+    private final InsuranceRepo insuranceRepo;
+    private final PatientRepo patientRepo;
+
+    public Patient assignInsuranceToPatient(Insurance insurance, Long patientId) {
+        Patient patient = patientRepo.findById(patientId)
+                .orElseThrow(() -> new EntityNotFoundException("Patient not found with ID: " + patientId));
+
+
+        patient.setInsurance(insurance);
+
+        return patient;
+    }
+}
